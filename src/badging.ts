213af1fd18b811b092de42114537ed03c539ac3c
Julian Hundeloh <julian@hundeloh-consulting.ch>
2019-05-21 16:15:07 +0200

    fix(src/badging.ts): do not use Badging if not used in standalone mode

--- src/badging.ts
+++ src/badging.ts
@@ -2,12 +2,15 @@ import { Value } from './index'
 
 const warn = () => {
   console.warn(
-    'Badging API is not enabled. Please check here how you can enable it: https://developers.google.com/web/updates/2018/12/badging-api#use'
+    'Application must run in standalone mode and Badging API must be enabled. Please check here how you can enable it: https://developers.google.com/web/updates/2018/12/badging-api#use'
   )
 }
 
 export function isAvailable() {
-  return 'ExperimentalBadge' in window
+  return (
+    window.matchMedia('(display-mode: standalone)').matches &&
+    'ExperimentalBadge' in window
+  )
 }
 
 export function set(value: Value) {
