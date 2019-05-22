4cb0731095da7df2b4ce001b18a235b3e826b98a
jaulz <jaulz@users.noreply.github.com>
2019-05-22 11:31:31 +0200

    fix(src/badging.ts): warn only once

--- src/badging.ts
+++ src/badging.ts
@@ -1,9 +1,15 @@
 import { Value } from './index'
 
+let warnedBefore = false
 const warn = () => {
+  if (warnedBefore) {
+    return
+  }
+
   console.warn(
     'Application must run in standalone mode and Badging API must be enabled. Please check here how you can enable it: https://developers.google.com/web/updates/2018/12/badging-api#use'
   )
+  warnedBefore = true
 }
 
 export function isAvailable() {
