44eba97b1d4d6feadeb02125f9662efbb3075a8e
jaulz <jaulz@users.noreply.github.com>
2019-05-22 12:04:43 +0200

    chore: bump version

--- build/index.es.js
+++ build/index.es.js
@@ -68,7 +68,7 @@ const context = canvas.getContext ? canvas.getContext('2d') : null
 const defaultOptions = {
   fontSize: 8 * ratio,
   fontFamily: 'Arial',
-  background: '#F03D25',
+  background: '#424242',
   color: '#ffffff',
   height: 8,
   width: 7,
