8b9cf8d5c7d30e2eed27a23460b65f5a9d7677c1
Julian Hundeloh <julian.hundeloh@novartis.com>
2019-09-10 14:02:16 +0200

    chore(build/): remove build folder

--- build/badging.d.ts
+++
@@ -1,5 +0,0 @@
-import { Value } from './index'
-export declare function isAvailable(): boolean
-export declare function set(value: Value): boolean
-export declare function clear(): void
-//# sourceMappingURL=badging.d.ts.map
--- build/isIndicator.d.ts
+++
@@ -1,3 +0,0 @@
-import { Value } from './index'
-export default function isIndicator(value: Value): boolean
-//# sourceMappingURL=isIndicator.d.ts.map
